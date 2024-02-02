1e86a4442817f72de1b43ca95ed860c99ad68a0a
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-02-02 11:14:21 +0100

    Initial commit

---
+++ src/test/java/com/svalero/supermercadoAPI/SupermercadoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svalero.supermercadoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermercadoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
